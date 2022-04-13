96335c7efa1b1f4919f1f9a8e92a4ea6e0fdac3b
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-04-13 17:05:36 -0400

    revised joey

---
+++ rebel/portfolio.js
@@ -0,0 +1,30 @@
+/*SLIDER*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+/*MENU TOGGLE*/
+const navToggle = document.querySelector('.nav__toggle');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+
+
+
