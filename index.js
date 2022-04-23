38d8ca46042bcc2eb062fa195fb55e901324adda
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-04-23 17:20:54 -0400

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+/*FADE IN WHILE SCROLLING*/
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.25,
+  rootMargin: "0px 0px -100px 0px"
+};
+
--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -3,3 +3,30 @@ const navToggle = document.querySelector('.nav__toggle');
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
+
+/*FADE IN WHILE SCROLLING*/
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.25,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
