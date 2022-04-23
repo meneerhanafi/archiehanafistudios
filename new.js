8e766cef992bb83eccd412f22716f92f1b332479
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-04-23 16:46:37 -0400

    update home

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-const navToggle = document.querySelector('.nav__toggle');
+const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
