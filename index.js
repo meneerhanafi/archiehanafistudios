cd4d54037ac9a726cc2e33ed77c20d02c62df695
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-05-07 18:09:38 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+
 const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
 
-/*FADE IN WHILE SCROLLING*/
-const faders = document.querySelectorAll(".fade-in");
 
-const appearOptions = {
-  threshold: 0.25,
-  rootMargin: "0px 0px -100px 0px"
-};
 
